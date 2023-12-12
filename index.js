225daf8f9e90c950e9cd68709e0f404bb60c29bb
TanvirCoU <ahmeadtanvir@stud.cou.ac.bd>
2023-12-13 01:27:32 +0600

    origin updated

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT || port);
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "https://panda-chat-webapp.netlify.app/"
+        origin: "https://panda-chat-webapp.netlify.app"
     },
 });
 
